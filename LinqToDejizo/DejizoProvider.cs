859add74cfb7e53ccad257545274d06f9bc844d8
NITTA Mitsuhiro <m_nitta@ap-com.co.jp>
2017-05-17 18:10:13 +0900

    リファクタリング

--- LinqToDejizo/DejizoProvider.cs
+++ LinqToDejizo/DejizoProvider.cs
@@ -81,39 +81,19 @@ namespace Marimo.LinqToDejizo
             var first = GetInfo<IQueryable<object>>(c => c.First()).GetGenericMethodDefinition();
             var single = GetInfo<IQueryable<object>>(c => c.Single()).GetGenericMethodDefinition();
 
-            switch (expression)
-            {
-                case MethodCallExpression m when new[] { count, first, single }.Contains(m.Method.GetGenericMethodDefinition()):
-                    condition.ResultType = m.Method.Name;
-                    ParseSelectItems(m.Arguments[0], condition);
-                    break;
-                case MethodCallExpression m:
-                    condition.ResultType = "SelectItems";
-                    ParseSelectItems(m, condition);
-                    break;
-
-            }
-        }
-
-        private  void ParseSelectItems(Expression expression, SearchDicItemCondition condition)
-        {
             var selectLambda = new LambdaParser();
 
             var unary = new UnaryParser { Operand = selectLambda };
 
-            selectLambda.Action = l => condition.SelectLambda = l;
-        
+
             var where = GetInfo<IQueryable<object>>(c => c.Where(x => true)).GetGenericMethodDefinition();
             var select = GetInfo<IQueryable<object>>(c => c.Select(x => x)).GetGenericMethodDefinition();
 
             var word = new ConstantParser();
-            word.Action = x => condition.Word = (string)x.Value;
+            
 
             var header = new MemberParser(x => x.Member.Name == "HeaderText");
-            header.Action = x =>
-            {
-                condition.Scope = "HEADWORD";
-            };
+            
             var binary1 = new BinaryParser()
             {
                 Left = word,
@@ -126,38 +106,22 @@ namespace Marimo.LinqToDejizo
             };
 
             var binary = new OrParser<BinaryExpression>(binary1, binary2);
-            binary.Action = _ =>
-            {
-                condition.Match = "EXACT";
-            };
+
 
             var call1 = new MethodCallParser(m => m.Method == GetInfo<string, string>((s, p) => s.EndsWith(p)))
             {
                 Arguments = new[] { word }
             };
 
-            call1.Action = m =>
-            {
-                condition.Match = "ENDWITH";
-                condition.Scope = "HEADWORD";
-            };
+
             var call2 = new MethodCallParser(m => m.Method == GetInfo<string, string>((s, p) => s.StartsWith(p)))
             {
                 Arguments = new[] { word }
             };
-            call2.Action = m =>
-            {
-                condition.Match = "STARTWITH";
-                condition.Scope = "HEADWORD";
-            };
+
             var call3 = new MethodCallParser(m => m.Method == GetInfo<string, string>((s, p) => s.Contains(p)))
             {
                 Arguments = new[] { word }
-            };
-            call3.Action = m =>
-            {
-                condition.Match = "CONTAIN";
-                condition.Scope = "HEADWORD";
             };
 
             var call = new OrParser<MethodCallExpression>(new OrParser<MethodCallExpression>(call1, call2), call3);
@@ -167,35 +131,60 @@ namespace Marimo.LinqToDejizo
                 Body = new OrParser(binary, call)
             };
 
-            var unary2= new UnaryParser
+            var unary2 = new UnaryParser
             {
                 Operand = lambda
             };
 
             var whereMethod = new MethodCallParser(m => m.Method.GetGenericMethodDefinition() == where)
             {
-                Arguments = new[] {null, unary2 }
+                Arguments = new[] { null, unary2 }
             };
-            var selectMethod = new MethodCallParser(m => m.Method.GetGenericMethodDefinition() == select);
-
-
-            whereMethod.Parse(expression);
-
-            switch (expression)
+            var selectMethod = new MethodCallParser(m => m.Method.GetGenericMethodDefinition() == select)
             {
-                //case MethodCallExpression m when m.Method.GetGenericMethodDefinition() == where:
-                //    unary2.Parse(m.Arguments[1]);
-                //    break;
-                case MethodCallExpression m when m.Method.GetGenericMethodDefinition() == select:
-                    switch (m.Arguments[0])
+                Arguments = new ExpressionParser[]
+                {
+                    new MethodCallParser
                     {
-                        case MethodCallExpression mm:
-                            unary2.Parse(mm.Arguments[1]);
-                            break;
-                    }
-                    unary.Parse(m.Arguments[1]);
-                    break;
-            }
+                        Arguments = new[]{null, unary2}
+                    },
+                    unary
+                }
+            };
+
+            var selectItems = new OrParser<MethodCallExpression>(whereMethod, selectMethod);
+
+            var lastMethodCall = new MethodCallParser(m => new[] { count, first, single }.Contains(m.Method.GetGenericMethodDefinition()))
+            {
+                Arguments = new[] { selectItems }
+            };
+
+            var whole = new OrParser(lastMethodCall, selectItems);
+
+
+            selectLambda.Action = l => condition.SelectLambda = l;
+            word.Action = x => condition.Word = (string)x.Value;
+            header.Action = x => condition.Scope = "HEADWORD";
+            binary.Action = _ => condition.Match = "EXACT";
+            call1.Action = m =>
+            {
+                condition.Match = "ENDWITH";
+                condition.Scope = "HEADWORD";
+            };
+            call2.Action = m =>
+            {
+                condition.Match = "STARTWITH";
+                condition.Scope = "HEADWORD";
+            };
+            call3.Action = m =>
+            {
+                condition.Match = "CONTAIN";
+                condition.Scope = "HEADWORD";
+            };
+            lastMethodCall.Action = m => condition.ResultType = m.Method.Name;
+            selectItems.Action = _ => condition.ResultType = "SelectItems";
+
+            whole.Parse(expression);
         }
 
         public abstract class ExpressionParser
